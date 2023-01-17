d3fedfb8aa76375695e8205f2ba960b1d7d61290
donydonald1 <donydonald1@icloud.com>
2023-01-17 07:11:16 -0600

    adds extra-manifests/kustomization.yaml.tpl

--- variables.tf
+++ variables.tf
@@ -1,5 +1,23 @@
 variable "hcloud_token" {
-  type = "string"
-  default = ""
-  description = "hcloud api token" 
+  type        = string
+  default     = ""
+  description = "hcloud api token"
+}
+
+variable "network_region" {
+  type = string
+  default = "us-east"
+  description = "hcloud network zone"
+}
+
+variable "location" {
+  type = string
+  default = "ash"
+  description = "hcloud location"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "devops2go-k3s-lab"
+  description = "hcloud k3s cluster name"
 }
